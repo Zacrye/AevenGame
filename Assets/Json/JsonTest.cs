c1225e2162671fc201da500b4a11146e5182a277
ComputerRat <christopher@homerunss.com>
2014-12-20 11:09:56 -0600

    Json code base

---
+++ Assets/Json/JsonTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class JsonTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//JsonSerializer.Serialize(Character, new Character());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
